ca3f2d0187a2e9b1171e5b94f108f15bb9696bcb
beardfriend <beardfriend21@gmail.com>
2022-04-28 23:30:47 +0900

    백준 6깅 10809

--- algorithm/backjoon/06/10809/10809.go
+++ algorithm/backjoon/06/10809/10809.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strconv"
+	"os"
 )
 
 func main() {
-	var input string
-	fmt.Scanln(&input)
-	var stringarray []string
 
-	
-	for i:= 0; i < len(input); i++ {
-		stringarray = append(stringarray, strconv.Itoa(i))
+	var s string
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Fscanln(reader, &s)
+
+	var alphabet = []string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+	var inputArray []string
+	for _,v:= range []rune(s) {
+		inputArray = append(inputArray, fmt.Sprintf("%c",v))	
 	}
-	fmt.Println(stringarray)
+	for i := 0; i < len(alphabet); i++ {
+		bol := false
+			for j:=0; j < len(inputArray); j++ {
+
+				if inputArray[j] == alphabet[i] {
+					bol = true
+					fmt.Printf("%d ", j)
+					break
+				} 
+
+			}
+			if !bol {
+				fmt.Print("-1 ")
+			}
+
+	}
+
 }
